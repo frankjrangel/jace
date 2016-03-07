e3f0f136ff20c9f3bf332a52c259cba3afd258c3
Francisco Rangel <frankjrangel@gmail.com>
2016-03-06 21:15:36 -0430

    Mapa, footer a medias

--- footer.php
+++ footer.php
@@ -1,8 +1,22 @@
 
 </div> <!-- content close -->
-
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <?php wp_footer(); ?>
+<footer>
+    <div class="logo">
 
+    </div>
+    <div class="oficinas">
+        <div class="oficina">
+
+        </div>
+        <div class="oficina">
+
+        </div>
+        <div class="oficina">
+
+        </div>
+    </div>
+</footer>
 </body>
 </html>
